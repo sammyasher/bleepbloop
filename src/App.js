80302239d6023aeb59060e824848cc9aaddde985
Sean Winnik <winnik.sean@gmail.com>
2024-02-07 22:31:48 -0800

    Smokey Sparkler

--- src/App.js
+++ src/App.js
@@ -1,16 +1,18 @@
-import React from 'react';
-import './App.css';
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import { Home } from './pages/Home';
+import React from "react";
+import "./App.css";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { Home } from "./pages/Home";
+import { SmokeySparkler } from "./pages/SmokeySparkler";
 
 const App = () => {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path='/' element={<Home />} />
+        <Route path="/" element={<Home />} />
+        <Route path="/smokeysparkler" element={<SmokeySparkler />} />
       </Routes>
     </BrowserRouter>
   );
-}
+};
 
 export default App;
