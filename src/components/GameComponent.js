de177e89baf47ca837c8fd0fc9119c7b95252343
sammyasher <67211951+sammyasher@users.noreply.github.com>
2024-02-07 23:30:22 -0500

    Add files via upload

---
+++ src/components/GameComponent.js
@@ -0,0 +1,17 @@
+import React, { useEffect } from "react"
+import Phaser from "phaser";
+
+export const GameComponent = ({ config }) => {
+    
+    useEffect(() => {
+        //create new phaser game upon component mount
+        const game = new Phaser.Game(config);
+        
+        //destroy game upon component unmount
+        return () => game.destroy(true);
+    });  
+
+    return (
+        <div id="phaser-container" />
+    );
+}
