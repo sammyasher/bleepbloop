2d6edf4ac1c5435ed49ea42dff18d20939c8bf19
Sean Winnik <winnik.sean@gmail.com>
2024-02-11 15:29:55 -0800

    more downParks

--- src/pages/MultiFireWork.js
+++ src/pages/MultiFireWork.js
@@ -26,23 +26,14 @@ class Example extends Phaser.Scene {
   create() {
     const groundToAir = this.add.particles(0, 0, "blue", {
       //all attributes: https://newdocs.phaser.io/docs/3.70.0/Phaser.Types.GameObjects.Particles.ParticleEmitterConfig
-
-      speed: 320, //{ min: 0, max: 1000}, //  100 (circles/shapes painting) // 1 or 0 (static shapes)
-      frequency: 200, // also try really fast speeds like 50, but reload over and over til it works. has to do with initialization problems. investigating.
+      speed: 320,
+      frequency: 200,
       lifespan: 2000,
-      scale: { start: 0.3, end: 0 }, //try single values, growth ones (smokey), and positive to negative which shrinks then grwos
-      //blendMode: "ADD",
+      scale: { start: 0.3, end: 0 },
       angle: { min: -100, max: -80 },
-      // delay: 1000,
-      // gravityX: 1000,
       gravityY: 150,
-      //moveToX: 100,  //activate both these to have particles sink into a specific point
-      //moveToY: 100,  //activate both these to have particles sink into a specific point
-      //maybe have first touch emit particles, second touch sink them into it
-
       follow: this.input.activePointer,
       emitting: false,
-
       deathCallback: (particle) => {
         secondaryFirework.explode(20, particle.x, particle.y);
       },
@@ -56,38 +47,28 @@ class Example extends Phaser.Scene {
       groundToAir.emitting = false;
     });
     const secondaryFirework = this.add.particles(0, 0, "red", {
-      speed: 50, //{ min: 0, max: 1000}, //  100 (circles/shapes painting) // 1 or 0 (static shapes)
-      frequency: 150, // also try really fast speeds like 50, but reload over and over til it works. has to do with initialization problems. investigating.
+      speed: 50,
+      frequency: 150,
       lifespan: 2000,
-      scale: { start: 0.3, end: 0 }, //try single values, growth ones (smokey), and positive to negative which shrinks then grwos
-
+      scale: { start: 0.3, end: 0 },
       gravityY: 30,
-
       emitting: false,
-
       deathCallback: (particle) => {
         tertiaryFirework.explode(6, particle.x, particle.y);
       },
     });
     const tertiaryFirework = this.add.particles(0, 0, "green", {
-      speed: 50, //{ min: 0, max: 1000}, //  100 (circles/shapes painting) // 1 or 0 (static shapes)
-      frequency: 400, // also try really fast speeds like 50, but reload over and over til it works. has to do with initialization problems. investigating.
+      speed: 50,
+      frequency: 400,
       lifespan: 3000,
-      scale: { start: 0.1, end: 0 }, //try single values, growth ones (smokey), and positive to negative which shrinks then grwos
-
+      scale: { start: 0.1, end: 0 },
       gravityY: 30,
-
       emitting: false,
-
-      deathCallback: (particle) => {
-        console.log("death callback");
-      },
     });
   }
 }
 
 export const MultiFireWork = () => {
-  //config
   const config = {
     type: Phaser.AUTO,
     parent: "phaser-container",
