9d82306b0dd18bc0067828c5cd2d48c5f520e9e3
Sean Winnik <winnik.sean@gmail.com>
2024-02-11 18:49:08 -0800

    4 color rainbow

--- src/pages/MultiFireWork.js
+++ src/pages/MultiFireWork.js
@@ -27,24 +27,38 @@ class Example extends Phaser.Scene {
     const groundToAir = this.add.particles(0, 0, "blue", {
       //all attributes: https://newdocs.phaser.io/docs/3.70.0/Phaser.Types.GameObjects.Particles.ParticleEmitterConfig
 
-      speed: 320, //{ min: 0, max: 1000}, //  100 (circles/shapes painting) // 1 or 0 (static shapes)
-      frequency: 200, // also try really fast speeds like 50, but reload over and over til it works. has to do with initialization problems. investigating.
+      speed: 320,
+      frequency: 200,
       lifespan: 2000,
-      scale: { start: 0.3, end: 0 }, //try single values, growth ones (smokey), and positive to negative which shrinks then grwos
-      //blendMode: "ADD",
+      scale: { start: 0.3, end: 0 },
+
       angle: { min: -100, max: -80 },
-      // delay: 1000,
-      // gravityX: 1000,
+
       gravityY: 150,
-      //moveToX: 100,  //activate both these to have particles sink into a specific point
-      //moveToY: 100,  //activate both these to have particles sink into a specific point
-      //maybe have first touch emit particles, second touch sink them into it
 
       follow: this.input.activePointer,
       emitting: false,
 
       deathCallback: (particle) => {
-        secondaryFirework.explode(20, particle.x, particle.y);
+        const colorOptions = [
+          secondaryRedFirework,
+          secondaryYellowFirework,
+          secondaryGreenFirework,
+          secondaryBlueFirework,
+        ];
+        const column = Math.floor(
+          (particle.x / window.innerWidth) * colorOptions.length
+        );
+        secondaryRedFirework.explode(20, particle.x, particle.y);
+
+        column === 0 &&
+          secondaryRedFirework.explode(20, particle.x, particle.y);
+        column === 1 &&
+          secondaryYellowFirework.explode(20, particle.x, particle.y);
+        column === 2 &&
+          secondaryGreenFirework.explode(20, particle.x, particle.y);
+        column >= 3 &&
+          secondaryBlueFirework.explode(20, particle.x, particle.y);
       },
     });
 
@@ -55,33 +69,117 @@ class Example extends Phaser.Scene {
     this.input.on("pointerup", () => {
       groundToAir.emitting = false;
     });
-    const secondaryFirework = this.add.particles(0, 0, "red", {
-      speed: 50, //{ min: 0, max: 1000}, //  100 (circles/shapes painting) // 1 or 0 (static shapes)
-      frequency: 150, // also try really fast speeds like 50, but reload over and over til it works. has to do with initialization problems. investigating.
+    const secondaryRedFirework = this.add.particles(0, 0, "red", {
+      speed: 50,
+      frequency: 150,
       lifespan: 2000,
-      scale: { start: 0.3, end: 0 }, //try single values, growth ones (smokey), and positive to negative which shrinks then grwos
-
+      scale: { start: 0.3, end: 0 },
       gravityY: 30,
-
       emitting: false,
-
       deathCallback: (particle) => {
-        tertiaryFirework.explode(6, particle.x, particle.y);
+        tertiaryRedFirework.explode(8, particle.x, particle.y);
+        // tertiarySparkle.explode(6, particle.x, particle.y);
       },
     });
-    const tertiaryFirework = this.add.particles(0, 0, "green", {
-      speed: 50, //{ min: 0, max: 1000}, //  100 (circles/shapes painting) // 1 or 0 (static shapes)
-      frequency: 400, // also try really fast speeds like 50, but reload over and over til it works. has to do with initialization problems. investigating.
+
+    const secondaryYellowFirework = this.add.particles(0, 0, "yellow", {
+      speed: 50,
+      frequency: 150,
+      lifespan: 2000,
+      scale: { start: 0.3, end: 0 },
+      gravityY: 30,
+      emitting: false,
+      deathCallback: (particle) => {
+        tertiaryYellowFirework.explode(8, particle.x, particle.y);
+        // tertiarySparkle.explode(6, particle.x, particle.y);
+      },
+    });
+
+    const secondaryGreenFirework = this.add.particles(0, 0, "green", {
+      speed: 50,
+      frequency: 150,
+      lifespan: 2000,
+      scale: { start: 0.3, end: 0 },
+      gravityY: 30,
+      emitting: false,
+      deathCallback: (particle) => {
+        tertiaryGreenFirework.explode(8, particle.x, particle.y);
+        // tertiarySparkle.explode(6, particle.x, particle.y);
+      },
+    });
+
+    const secondaryBlueFirework = this.add.particles(0, 0, "blue", {
+      speed: 50,
+      frequency: 150,
+      lifespan: 2000,
+      scale: { start: 0.3, end: 0 },
+      gravityY: 30,
+      emitting: false,
+      deathCallback: (particle) => {
+        tertiaryBlueFirework.explode(8, particle.x, particle.y);
+        // tertiarySparkle.explode(6, particle.x, particle.y);
+      },
+    });
+
+    const tertiaryRedFirework = this.add.particles(0, 0, "red", {
+      speed: 50,
+      frequency: 400,
       lifespan: 3000,
-      scale: { start: 0.1, end: 0 }, //try single values, growth ones (smokey), and positive to negative which shrinks then grwos
+      scale: { start: 0.1, end: 0 },
 
       gravityY: 30,
 
       emitting: false,
 
-      deathCallback: (particle) => {
-        console.log("death callback");
-      },
+      deathCallback: (particle) => {},
+    });
+    const tertiaryYellowFirework = this.add.particles(0, 0, "yellow", {
+      speed: 50,
+      frequency: 400,
+      lifespan: 3000,
+      scale: { start: 0.1, end: 0 },
+
+      gravityY: 30,
+
+      emitting: false,
+
+      deathCallback: (particle) => {},
+    });
+    const tertiaryGreenFirework = this.add.particles(0, 0, "green", {
+      speed: 50,
+      frequency: 400,
+      lifespan: 3000,
+      scale: { start: 0.1, end: 0 },
+
+      gravityY: 30,
+
+      emitting: false,
+
+      deathCallback: (particle) => {},
+    });
+    const tertiaryBlueFirework = this.add.particles(0, 0, "blue", {
+      speed: 50,
+      frequency: 400,
+      lifespan: 3000,
+      scale: { start: 0.1, end: 0 },
+
+      gravityY: 30,
+
+      emitting: false,
+
+      deathCallback: (particle) => {},
+    });
+    const tertiarySparkle = this.add.particles(0, 0, "sparkle", {
+      speed: 50,
+      frequency: 400,
+      lifespan: 3000,
+      scale: { start: 0.1, end: 0 },
+
+      gravityY: 30,
+
+      emitting: false,
+
+      deathCallback: (particle) => {},
     });
   }
 }
