bc8a524603b3568b54e9f12d2a3048ff2bfdcc70
Sean Winnik <winnik.sean@gmail.com>
2024-02-23 08:20:10 -0800

    hyperstimulus functional

--- src/pages/routes/index.js
+++ src/pages/routes/index.js
@@ -1,3 +1,5 @@
+import { useNavigate } from "react-router-dom";
+
 import { Home } from "../Home";
 import { Sparkler } from "../Sparkler";
 import { SmokeySparklerCone } from "../SmokeySparklerCone";
@@ -11,32 +13,82 @@ import { BeanBoy } from "../BeanBoy";
 import { GlowTone } from "../GlowTone";
 import { Glorp } from "../Glorp";
 import { SpringThing } from "../SpringThing";
-import { useNavigate } from "react-router-dom";
-  
+import { HyperStimulus } from "../HyperStimulus";
 
 const paths = [
-  { to: "/", element: <Home />, includeInNav: true, name: "Home"},
-  { to: "/sparkler", element: <Sparkler />, includeInNav: true, name: "Sparkler"},
-  { to: "/smokeysparkler-cone", element: <SmokeySparklerCone />, includeInNav: true, name: "SmokeySparklerCone"},
-  { to: "/notemaker", element: <NoteMaker />, includeInNav: true, name: "NoteMaker"},
-  { to: "/samoji", element: <Samoji />, includeInNav: true, name: "Samoji"},
-  { to: "/sourceandsink", element: <SourceAndSink />, includeInNav: true, name: "SourceAndSink"},
-  { to: "/multifirework", element: <MultiFireWork />, includeInNav: true, name: "MultiFireWork"},
-  { to: "/circlebouncetone", element: <CircleBounceTone />, includeInNav: false, name: "CircleBounceTone"},
-  { to: "/beanboy", element: <BeanBoy />, includeInNav: true, name: "BeanBoy"},
-  { to: "/glowtone", element: <GlowTone />, includeInNav: false, name: "GlowTone"},
-  { to: "/Glorp", element: <Glorp />, includeInNav: true, name: "Glorp"},
-  { to: "/springthing", element: <SpringThing />, includeInNav: true, name: "SpringThing"}
-
+  { to: "/", element: <Home />, includeInNav: true, name: "Home" },
+  {
+    to: "/sparkler",
+    element: <Sparkler />,
+    includeInNav: true,
+    name: "Sparkler",
+  },
+  {
+    to: "/smokeysparkler-cone",
+    element: <SmokeySparklerCone />,
+    includeInNav: true,
+    name: "SmokeySparklerCone",
+  },
+  {
+    to: "/notemaker",
+    element: <NoteMaker />,
+    includeInNav: true,
+    name: "NoteMaker",
+  },
+  { to: "/samoji", element: <Samoji />, includeInNav: true, name: "Samoji" },
+  {
+    to: "/sourceandsink",
+    element: <SourceAndSink />,
+    includeInNav: true,
+    name: "SourceAndSink",
+  },
+  {
+    to: "/multifirework",
+    element: <MultiFireWork />,
+    includeInNav: true,
+    name: "MultiFireWork",
+  },
+  {
+    to: "/circlebouncetone",
+    element: <CircleBounceTone />,
+    includeInNav: false,
+    name: "CircleBounceTone",
+  },
+  { to: "/beanboy", element: <BeanBoy />, includeInNav: true, name: "BeanBoy" },
+  {
+    to: "/glowtone",
+    element: <GlowTone />,
+    includeInNav: false,
+    name: "GlowTone",
+  },
+  { to: "/Glorp", element: <Glorp />, includeInNav: true, name: "Glorp" },
+  {
+    to: "/springthing",
+    element: <SpringThing />,
+    includeInNav: true,
+    name: "SpringThing",
+  },
+  {
+    to: "/hyperstimulus",
+    element: <HyperStimulus />,
+    includeInNav: true,
+    name: "HyperStimulus",
+  },
 ];
 
 const Dropdown = () => {
-  const navigate = useNavigate(); 
+  const navigate = useNavigate();
 
   return (
-    <select onChange={(event) => {navigate(event.target.value)}}>   
-      {paths.filter((path) => path.includeInNav).map((path) => (
-          <option value={path.to}>{path.name}</option> 
+    <select
+      onChange={(event) => {
+        navigate(event.target.value);
+      }}
+    >
+      {paths
+        .filter((path) => path.includeInNav)
+        .map((path) => (
+          <option value={path.to}>{path.name}</option>
         ))}
     </select>
   );
@@ -45,7 +97,7 @@ const Dropdown = () => {
 const navLinks = <Dropdown />;
 
 const routes = paths.map((path) => (
-  <Route path={path.to} element={path.element} />  
+  <Route path={path.to} element={path.element} />
 ));
 
 export { paths, navLinks, routes };
